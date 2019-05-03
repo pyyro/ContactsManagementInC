aa1c114049f7a1f5d1b84ebcf0d5f4022ae0e1d9
pyyro <30564925+pyyro@users.noreply.github.com>
2019-05-03 15:05:27 +0545

    only some ui changes

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include<string.h>
 #include<stdlib.h>
 #include<stdbool.h>
+
 // Contacts structure
 struct contact {
     char name[40];
@@ -11,7 +12,7 @@ struct contact {
     char email[40];
 };
 
-//Functions List
+//Functions Prototypes
 void mainMenu();
 void addRecord();
 void delRecord();
@@ -25,8 +26,8 @@ char ftempname[]={"temp.dll"};
 
 //__main__
 int main(){
-    system("color 06"); //colours wwoahh!!
-                        /* First X for background, second for foreground.
+    system("color 06"); 
+                        /* colors woah First X for background, second for foreground.
                         0 = Black   8 = Gray
                         1 = Blue    9 = Light Blue
                         2 = Green   A = Light Green
@@ -34,7 +35,7 @@ int main(){
                         4 = Red     C = Light Red
                         5 = Purple  D = Light Purple
                         6 = Yellow  E = Light Yellow
-                        7 = White   F = Bright White 
+                        7 = White   F = Bright White
                         */
    mainMenu();
    return 0;
@@ -45,9 +46,13 @@ void mainMenu(){
     system("cls"); //clear screen before every call so it looks neeeat
 
     printf("\t\t\t\t\t\tMAIN MENU\n");
-    printf("\t\t1.Add New Contact\t2.Edit Existing Contact\t\t3.Delete Contact\n");
-    printf("\t\t4.Search Contact\t5.List All Contacts\t\t6.Exit Program\n\n");
-    printf("\t\t\tChoose options by pressing their respective numbers!\n");
+    printf("\n\n\t\t\t\t1. Add New Contact.");
+    printf("\n\t\t\t\t2. Edit Existing Contact.");
+    printf("\n\t\t\t\t3. Delete Contact.");
+    printf("\n\t\t\t\t4. Search Contacts.");
+    printf("\n\t\t\t\t5. List All  Contacts.");
+    printf("\n\t\t\t\t6. Exit Program.");
+
     
     switch(getch()){
         case '1': addRecord();
@@ -63,8 +68,8 @@ void mainMenu(){
         case '6': system("exit");
                   break;
         default:
-            printf("Press 1-6 only!!");
-            printf("\nPress any key to try again.");
+            printf("\n\t\t\tPress 1-6 only!!");
+            printf("\n\t\t\tPress any key to try again.");
             getch();
     
     mainMenu();
@@ -81,17 +86,28 @@ void addRecord(){
     FILE *f;
     f=fopen(fname,"ab");
     
-    printf("\t\tADD NEW CONTACT\n");
-    printf("\nEnter name: ");
-    scanf("%[^\n]",&c.name);// so it looks like the %[^\n] waits until I press Enter to save the data so the last name doesnt get lost or overflow
+    printf("\t\t\t========================================\n");
+    printf("\t\t\t\t\tAdd Contact\n");
+    printf("\t\t\t========================================\n");
+    printf("\n\t\t\tEnter name: ");
+    scanf("%[^\n]",&c.name);// so it looks like the %[^\n] waits until I press Enter to save the data so the last name doesnt get lost or overflows, im not sure
+    for (int i =48;i<57;i++){
+        if(*c.name==i){
+            printf("\t\t\tPlease enter a valid name!");
+            printf("\n\t\t\tPress any key to try again!");
+            getch();
+            fflush(stdin);
+            addRecord();
+        }
+    }
     fflush(stdin); //apparently clears buffer so no value overflows, solved my problem so whatever 
-    printf("\nEnter address: ");
+    printf("\n\t\t\tEnter address: ");
     scanf("%[^\n]",&c.address);
     fflush(stdin);
-    printf("\nEnter phone number: ");
+    printf("\n\t\t\tEnter phone number: ");
     scanf("%lld",&c.phoneNumber);
     fflush(stdin);
-    printf("\nEnter email address: ");
+    printf("\n\t\t\tEnter email address: ");
     scanf("%s",&c.email);
     fflush(stdin);
     
@@ -114,6 +130,7 @@ void listRecords(){
     FILE *f;
     f=fopen(fname,"rb"); //binary read mode
 
+    //i probably dont need to do this 
     if (f==NULL){
         printf("\nERROR OPENING FILE, THE RECORD FILE DOES NOT EXIST!!!");
         getch();
@@ -122,14 +139,14 @@ void listRecords(){
         printf("\t\t\t========================================\n");
         printf("\t\t\t\t\tContacts\n");
         printf("\t\t\t========================================\n");
-        while(fread(&c,sizeof(c),1,f)==1){ //okay this reads the file until it reaches the end, and when it does its return value is (i don't know).
+        while(fread(&c,sizeof(c),1,f)==1){ //okay this reads the file until it reaches the end, and when it does its return value is 0. (as always im not sure about this)
         printf("\n\t\t\t\tName: %s\n\t\t\t\tAddress: %s\n\t\t\t\tMobile no: %lld\n\t\t\t\tE-mail: %s\n\n\t\t\t\t\t  --\n\n",c.name,c.address,c.phoneNumber,c.email);
         }
     }
     
     fclose(f);
     
-    printf("\nPress any key to go back!");
+    printf("\n\t\t\tPress any key to go back!");
     getch();
     mainMenu();
 }
@@ -157,8 +174,10 @@ void delRecord(){
     fp = fopen(fname,"r");
     ftemp = fopen(ftempname,"w");
     
-    printf("\t\tDELETE CONTACT\n");
-    printf("Enter name of contact to delete: ");
+    printf("\t\t\t========================================\n");
+    printf("\t\t\t\t\tDelete Contact\n");
+    printf("\t\t\t========================================\n");
+    printf("\t\t\tEnter name of contact to delete: ");
     scanf("%[^\n]",&name);
     fflush(stdin);
     
@@ -173,14 +192,14 @@ void delRecord(){
     fclose(ftemp);
     
     if(recordExists!=true){
-        printf("The contact does not exist!");
+        printf("\t\t\tThe contact does not exist!");
     }else{
         remove(fname);
         rename(ftempname, fname);
-        printf("\t\tRECORD DELETED SUCCESSFULLY.");
+        printf("\t\t\t\t\tRECORD DELETED SUCCESSFULLY.");
     }
    
-    printf("\n\nPress any key to go back!");
+    printf("\n\n\t\t\tPress any key to go back!");
     getch();
     system("cls");
     
@@ -201,7 +220,9 @@ void editRecord(){
 
     system("cls");
     
-    printf("\t\tEDIT CONTACT\n");
+    printf("\t\t\t========================================\n");
+    printf("\t\t\t\t\tEdit Contact\n");
+    printf("\t\t\t========================================\n");
     fflush(stdin);
     
     struct contact c;
@@ -211,7 +232,7 @@ void editRecord(){
     fp = fopen(fname,"r");
     ftemp = fopen(ftempname,"w");
 
-    printf("Enter name of contact to modify: ");
+    printf("\t\t\tEnter name of contact to modify: ");
     scanf("%[^\n]",&name);
     fflush(stdin);
 
@@ -223,21 +244,31 @@ void editRecord(){
             }
     }
     if (found == 1){
-        printf("\nEnter name: ");
+        restart:
+        printf("\n\t\t\tEnter name: ");
         scanf("%[^\n]",&c.name);
+        for (int i =48;i<57;i++){
+            if(*c.name==i){
+                printf("\t\t\tPlease enter a valid name!");
+                printf("\n\t\t\tPress any key to try again!");
+                getch();
+                fflush(stdin);
+                goto restart;
+            }
+        }
         fflush(stdin); 
-        printf("\nEnter address: ");
+        printf("\n\t\t\tEnter address: ");
         scanf("%s",&c.address);
         fflush(stdin);
-        printf("\nEnter phone number: ");
+        printf("\n\t\t\tEnter phone number: ");
         scanf("%lld",&c.phoneNumber);
         fflush(stdin);
-        printf("\nEnter email address: ");
+        printf("\n\t\t\tEnter email address: ");
         scanf("%s",&c.email);
         fflush(stdin);
         fwrite(&c,sizeof(c),1,ftemp);
     }else if(found!=1){
-        printf("The contact does not exist!");
+        printf("\t\t\tThe contact does not exist!");
     }
     
 
@@ -247,7 +278,7 @@ void editRecord(){
     remove(fname);
     rename(ftempname,fname);
 
-    printf("\n\nPress any key to go back!");
+    printf("\n\n\t\t\tPress any key to go back!");
     getch();
     system("cls");
     
@@ -268,25 +299,27 @@ void searchRecords(){
     
     system("cls");
     
-    printf("=========SEARCH CONTACT============\n");
-    printf("Enter name of contact to search: ");
+    printf("\t\t\t========================================\n");
+    printf("\t\t\t\t\tEdit Contact\n");
+    printf("\t\t\t========================================\n");
+    printf("\t\t\tEnter name of contact to search: ");
     scanf("%[^\n]",&name);
     fflush(stdin);
 
     while (fread( &c,sizeof(c),1,fp)==1){
         if (strcmp(c.name,name)==0){
             found = 1;
-            printf("\n\tInfo about: %s",name);
-            printf("\nName: %s\nAddress: %s\nMobile no: %lld\nE-mail: %s\n\n",c.name,c.address,c.phoneNumber,c.email);
+            printf("\n\t\t\t\tInfo about: %s",name);
+            printf("\n\t\t\tName: %s\n\t\t\tAddress: %s\n\t\t\tPhone No.: %lld\n\t\t\tE-mail: %s\n\n",c.name,c.address,c.phoneNumber,c.email);
             break;
         }
     }
 
     fclose(fp);
     if (found!=1){
-        printf("No contact matches for that name!");
+        printf("\t\t\tNo contact matches for that name!");
     }
-    printf("\n\nPress any key to go back!");
+    printf("\n\n\t\t\tPress any key to go back!");
 
     getch();
     mainMenu();
